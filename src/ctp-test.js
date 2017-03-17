92ef35658ca434d0ee6f1d6b4827054eb80b390e
Thomas Clavier <tom@tcweb.org>
2017-03-17 22:41:24 +0100

    Q2

--- src/ctp-test.js
+++ src/ctp-test.js
@@ -1,14 +1,33 @@
 const ctp = require('ctp');
+var jsdom = require("jsdom");
+var document = jsdom.jsdom(undefined);
+global.document = document;
 const assert = require('assert');
 
-describe('Q1', () => {
-  it('createWorld should create world map initialised by Undefined', () => {
-    assert.equal(undefined, ctp.createWorld(2, 2)[1][1]);
+describe('Control TP', () => {
+  describe('Q1', () => {
+    it('createWorld should create world map initialised by Undefined', () => {
+      assert.equal(undefined, ctp.createWorld(2, 2)[1][1]);
+    });
+    it('createWorld should create world map with good lines number', () => {
+      assert.equal(2, ctp.createWorld(2, 2).length );
+    });
+    it('createWorld should create world map with good column number', () => {
+      assert.equal(2, ctp.createWorld(2, 2)[0].length );
+    });
   });
-  it('createWorld should create world map with good lines number', () => {
-    assert.equal(2, ctp.createWorld(2, 2).length );
-  });
-  it('createWorld should create world map with good column number', () => {
-    assert.equal(2, ctp.createWorld(2, 2)[0].length );
+
+  describe('Q2', () => {
+    it('buildHtmlTable should generate a html table with u for undefined cell', () => {
+      const world = [[undefined, undefined],
+                     [undefined, undefined]];
+      assert.equal("u", ctp.buildHtmlTable(world).rows[1].cells[1].innerHTML);
+    });
+    it('buildHtmlTable should generate a html table with good line number', () => {
+      const world = [[undefined, undefined],
+                     [undefined, undefined]];
+      assert.equal(2, ctp.buildHtmlTable(world).rows.length);
+    });
+
   });
 });
--- src/ctp.js
+++ src/ctp.js
@@ -1,9 +1,59 @@
 'use strict';
 
-module.exports.createWorld = function (line, column) {
+var createWorld = function (line, column) {
   var map = new Array(line);
   for (var i = 0; i < line; i++) {
    map[i]=new Array(column);
   }
   return map;
+};
+
+var buildHtmlTable = function (world) {
+  var table = document.createElement('table');
+  for (var line_number = 0; line_number < world.length; line_number++) {
+    const line = world[line_number];
+    table.appendChild(buildHtmlLine(line));
+  }
+  return table;
+};
+
+var buildHtmlLine = function(line) {
+  var html_line = document.createElement('tr');
+  for (var cell_id = 0; cell_id < line.length; cell_id++) {
+    var html_cell = document.createElement('td');
+    html_cell.innerHTML = 'u';
+    html_line.appendChild(html_cell);
+  }
+  return html_line;
 }
+
+var createTable = function(world) {
+  const table = buildHtmlTable(world);
+  const text_screen = document.getElementById('text_screen');
+  text_screen.appendChild(table);
+};
+
+/*
+(function(exports){
+  exports.createWorld = createWorld;
+}(typeof exports === 'undefined' ? this.ctp = {} : exports));
+*/
+
+if (typeof window === 'undefined') {
+  module.exports.createWorld = createWorld;
+  module.exports.buildHtmlTable = buildHtmlTable;
+} else {
+  window.createWorld = createWorld;
+  window.createTable = createTable;
+}
+
+/*
+(function(window){
+    if (typeof module === "object" && module && typeof module.exports === "object") {
+      module.exports.createWorld = createWorld;
+    } else {
+      window.createWorld = createWorld;
+      window.createTable = createTable;
+    }
+})(this)
+*/
